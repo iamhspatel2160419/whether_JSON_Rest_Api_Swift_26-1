922c121b0b3b4b65d545a7f5ca15979b896074d7
hp ios <hpios@hps-Mac.local>
2018-01-25 07:27:23 -0800

    Initial Commit

---
+++ whether_JSON_Rest_Api/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whether_JSON_Rest_Api
+//
+//  Created by hp ios on 1/25/18.
+//  Copyright © 2018 andiosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
